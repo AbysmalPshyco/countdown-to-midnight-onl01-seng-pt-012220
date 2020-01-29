b39902e73ab8d34c5d319cb3039add3b0d289f06
Anthony Risch <rischthefisch@gmail.com>
2020-01-29 19:55:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,11 +9,11 @@ def countdown(number)
 end
 countdown(10)
 
-# def countdown_with_sleep
+def countdown_with_sleep(number)
   
-# sleep 2.00 seconds  while number > 0 
-#   puts "#{number} SECOND(S)!"
-#     number -= 1 
-#   end
-# end
-# countdown_with_sleep(10)
+sleep 2.00 seconds  while number > 0 
+  puts "#{number} SECOND(S)!"
+    number -= 1 
+  end
+end
+countdown_with_sleep(10)
